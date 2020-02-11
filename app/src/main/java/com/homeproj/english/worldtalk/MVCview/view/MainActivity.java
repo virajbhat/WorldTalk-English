ecaf8ad50c089af4f60f1e9e7de77e4ef811588f
virajbhat <virajbhat69@gmail.com>
2020-02-11 13:33:41 +0000

    Splash Screen working fine. (2 sec time)

---
+++ app/src/main/java/com/homeproj/english/worldtalk/MVCview/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.homeproj.english.worldtalk.MVCview.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.homeproj.english.worldtalk.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
